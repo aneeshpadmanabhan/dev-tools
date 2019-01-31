735eb1e8d47ed10de69f90f37a4cdd62ecd66c67
lkhusid <lkhusid@walmartlabs.com>
2019-01-31 13:23:48 -0500

    Reworked inductor stub.

--- inductor-stub/src/main/java/com/oneops/inductor/stub/WoListener.java
+++ inductor-stub/src/main/java/com/oneops/inductor/stub/WoListener.java
@@ -16,8 +16,18 @@
  *******************************************************************************/
 package com.oneops.inductor.stub;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.oneops.cms.cm.ops.domain.OpsActionState;
+import com.oneops.cms.simple.domain.CmsActionOrderSimple;
+import com.oneops.cms.simple.domain.CmsCISimple;
+import com.oneops.cms.simple.domain.CmsRfcCISimple;
+import com.oneops.cms.simple.domain.CmsWorkOrderSimple;
+import com.oneops.cms.util.CmsUtil;
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -25,162 +35,142 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.gson.Gson;
-import com.oneops.cms.cm.ops.domain.OpsActionState;
-import com.oneops.cms.domain.CmsWorkOrderSimpleBase;
-import com.oneops.cms.simple.domain.CmsActionOrderSimple;
-import com.oneops.cms.simple.domain.CmsCISimple;
-import com.oneops.cms.simple.domain.CmsRfcCISimple;
-import com.oneops.cms.simple.domain.CmsWorkOrderSimple;
-
 public class WoListener implements MessageListener {
-    private MessageProducer producer;
 
-    public MessageProducer getProducer() {
-        return producer;
-    }
+  private final int listenerId;
+  private final Gson gson = new Gson();
+  private final Session session;
+  private final MessageProducer producer;
+  private final String[] stateCycle;
+  private final long sleepTime;
 
-    public void setProducer(MessageProducer producer) {
-        this.producer = producer;
-    }
+  private int stateCycleCounter = 0;
 
-    public String getStatus() {
-        return status;
-    }
+  public WoListener(int id, Session session, MessageProducer producer, String stateCycle, long sleepTime) {
+    this.listenerId = id;
+    this.session = session;
+    this.producer = producer;
+    this.stateCycle = stateCycle.split(",");
+    this.sleepTime = sleepTime;
+  }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+  @Override
+  public void onMessage(Message msg) {
+    try {
+      if (msg instanceof TextMessage) {
+        String corelationId = msg.getJMSCorrelationID();
+        String type = msg.getStringProperty("type");
+        boolean isBatch = msg.getBooleanProperty("batch");
 
-    public long getSleepTime() {
-        return sleepTime;
-    }
-
-    public void setSleepTime(long sleepTime) {
-        this.sleepTime = sleepTime;
-    }
-
-    public Session getSession() {
-        return session;
-    }
-
-    public void setSession(Session session) {
-        this.session = session;
-    }
-
-    private String status;
-    private long sleepTime;
-    final private Gson gson = new Gson();
-    private Session session = null;
-
-    private static final Logger LOG = LoggerFactory.getLogger(WoListener.class);
-
-    @Override
-    public void onMessage(Message msg) {
-        try {
-            if (msg instanceof TextMessage) {
-                //LOG.info("got message: " + ((TextMessage)msg).getText());
-                String corelationId = msg.getJMSCorrelationID();
-                String type = msg.getStringProperty("type");
-
-                CmsWorkOrderSimpleBase wo;
-                if (type.equals("opsprocedure")) {
-                    wo = gson.fromJson(((TextMessage) msg).getText(), CmsActionOrderSimple.class);
-                    LOG.info("got message: actionId = " + ((CmsActionOrderSimple) wo).getActionId());
-                } else {
-                    wo = gson.fromJson(((TextMessage) msg).getText(), CmsWorkOrderSimple.class);
-                    LOG.info("got message: rfcId = " + ((CmsWorkOrderSimple) wo).rfcCi.getRfcId());
-                }
-
-
-                //Assuming we forked msg process hew ACK the MSG
-                //msg.acknowledge();
-                /*
-		    	if (wo.getServices() != null) {
-		    		System.out.println("Services for this wo = " + gson.toJson(wo.getServices()));
-		    	}
-		    	*/
-
-                Thread.sleep(this.sleepTime);
-
-                TextMessage responseMsg = session.createTextMessage();//(gson.toJson(wo));
-                responseMsg.setStringProperty("type", type);
-                if (type.equals("opsprocedure")) {
-                    processOpsProcedure(wo, responseMsg);
-                } else {
-                    processDeployment(wo, responseMsg);
-                }
-
-                responseMsg.setText(gson.toJson(wo));
-                responseMsg.setJMSCorrelationID(corelationId);
-                producer.send(responseMsg);
-                session.commit();
-            }
-        } catch (JMSException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-
-        }
-    }
-
-    private void processOpsProcedure(CmsWorkOrderSimpleBase wob, TextMessage responseMsg) throws JMSException {
-        CmsActionOrderSimple wo = (CmsActionOrderSimple) wob;
-        if (status.equalsIgnoreCase("complete")) {
-            wo.setActionState(OpsActionState.complete);
-            CmsCISimple resultCi = buildCi(wo.getCi());
-            if (resultCi.getCiAttributes().containsKey("max_instances")) {
-                resultCi.getCiAttributes().put("max_instances", "1500");
-            }
-            wo.setResultCi(resultCi);
-            responseMsg.setStringProperty("task_result_code", "200");
+        if (type.equals(CmsUtil.ACTION_ORDER_TYPE)) {
+          CmsActionOrderSimple ao = gson.fromJson(((TextMessage) msg).getText(), CmsActionOrderSimple.class);
+          log("received action order " + ao.getActionId());
+          Thread.sleep(this.sleepTime);
+          processActionOrder(ao, corelationId, stateCycle[stateCycleCounter % stateCycle.length]);
+          log("processed action order " + ao.getActionId() + " - " + ao.getActionState().getName().toUpperCase());
+          stateCycleCounter++;
         } else {
-            wo.setActionState(OpsActionState.failed);
-            responseMsg.setStringProperty("task_result_code", "404");
-        }
-        responseMsg.setStringProperty("type", "opsprocedure");
-    }
+          List<CmsWorkOrderSimple> wos;
+          if (isBatch) {
+            wos = gson.fromJson(((TextMessage) msg).getText(), (new TypeToken<ArrayList<CmsWorkOrderSimple>>() {
+            }).getType());
+            log("received work order batch of " + wos.size());
+          } else {
+            wos = new ArrayList<>(1);
+            CmsWorkOrderSimple wo = gson.fromJson(((TextMessage) msg).getText(), CmsWorkOrderSimple.class);
+            wos.add(wo);
+            log("received work order " + wo.rfcCi.getRfcId());
+          }
 
-    private void processDeployment(CmsWorkOrderSimpleBase wob, TextMessage responseMsg) throws JMSException {
-        CmsWorkOrderSimple wo = (CmsWorkOrderSimple) wob;
-        if (status.equalsIgnoreCase("complete")) {
-            CmsCISimple resultCi = buildCi(wo.getRfcCi());
-            wo.setResultCi(resultCi);
-            wo.setDpmtRecordState("complete");
-            responseMsg.setStringProperty("task_result_code", "200");
-        } else {
-            wo.setDpmtRecordState("failed");
-            responseMsg.setStringProperty("task_result_code", "404");
-            wo.setComments("Failed by inductor stub");
+          Console console = System.console();
+          for (int i = 0, batchSize = wos.size(); i < batchSize; i++) {
+            String state;
+            state = stateCycle[stateCycleCounter % stateCycle.length];
+            if (state.equals("?") || state.equals("prompt")) {
+              state = console.readLine("Complete or Fail? ('c', 'f'): ").toLowerCase().startsWith("c") ? "complete" : "failed";
+            }
+            Thread.sleep(this.sleepTime);
+            CmsWorkOrderSimple wo = wos.get(i);
+            CmsRfcCISimple rfc = wo.rfcCi;
+            processWorkOrder(wo, corelationId, state);
+            log("processed work order " + rfc.getRfcId() + " - " + state.toUpperCase() + " (" + rfc.getCiClassName() + " " + rfc.getCiName() + ")");
+            if (!"complete".equals(state) && i < batchSize - 1) {
+              log("forcing rest of work order batch  - " + state.toUpperCase());
+              for (int j = i + 1; j < batchSize; j ++) {
+                processWorkOrder(wos.get(j), corelationId, state);
+              }
+              i = batchSize;  // to break out of the loop
+            }
+            stateCycleCounter++;
+          }
         }
-        responseMsg.setStringProperty("type", "deploybom");
+      }
+    } catch (Throwable e) {
+      log("Failed to process: " + msg);
+      e.printStackTrace();
     }
+  }
 
-    private CmsCISimple buildCi(CmsRfcCISimple rfc) {
-        CmsCISimple ci = new CmsCISimple();
-        ci.setCiId(rfc.getCiId());
-        ci.setCiClassName(rfc.getCiClassName());
-        ci.getAttrProps().putAll(rfc.getCiAttrProps());
-        for (Map.Entry<String, String> attr : rfc.getCiAttributes().entrySet()) {
-                ci.addCiAttribute(attr.getKey(), attr.getValue());
-        }
-        return ci;
+  private void processActionOrder(CmsActionOrderSimple ao, String corelationId, String state) throws JMSException {
+    TextMessage responseMsg = session.createTextMessage();
+    responseMsg.setJMSCorrelationID(corelationId);
+    responseMsg.setStringProperty("type", CmsUtil.ACTION_ORDER_TYPE);
+
+    if (state.equalsIgnoreCase("complete")) {
+      ao.setActionState(OpsActionState.complete);
+      CmsCISimple inputCi = ao.getCi();
+      CmsCISimple ci = new CmsCISimple();
+      ci.setCiId(inputCi.getCiId());
+      ci.setCiClassName(inputCi.getCiClassName());
+      ci.getAttrProps().putAll(inputCi.getAttrProps());
+      for (Map.Entry<String, String> attr : inputCi.getCiAttributes().entrySet()) {
+        ci.addCiAttribute(attr.getKey(), attr.getValue());
+      }
+      if (ci.getCiAttributes().containsKey("max_instances")) {
+        ci.getCiAttributes().put("max_instances", "1500");
+      }
+      ao.setResultCi(ci);
+      responseMsg.setStringProperty("task_result_code", "200");
+    } else {
+      ao.setActionState(OpsActionState.failed);
+      responseMsg.setStringProperty("task_result_code", "404");
     }
+    responseMsg.setStringProperty("type", "opsprocedure");
+    responseMsg.setText(gson.toJson(ao));
+    producer.send(responseMsg);
+    session.commit();
+  }
 
-    private CmsCISimple buildCi(CmsCISimple inputCi) {
-        CmsCISimple ci = new CmsCISimple();
-        ci.setCiId(inputCi.getCiId());
-        ci.setCiClassName(inputCi.getCiClassName());
-        ci.getAttrProps().putAll(inputCi.getAttrProps());
-        for (Map.Entry<String, String> attr : inputCi.getCiAttributes().entrySet()) {
-                ci.addCiAttribute(attr.getKey(), attr.getValue());
-        }
-        return ci;
+
+  private void processWorkOrder(CmsWorkOrderSimple wo, String corelationId, String status) throws JMSException {
+    TextMessage responseMsg = session.createTextMessage();
+    responseMsg.setJMSCorrelationID(corelationId);
+    responseMsg.setStringProperty("type", CmsUtil.WORK_ORDER_TYPE);
+
+    if (status.equalsIgnoreCase("complete")) {
+      CmsRfcCISimple rfc = wo.getRfcCi();
+      CmsCISimple ci = new CmsCISimple();
+      ci.setCiId(rfc.getCiId());
+      ci.setCiClassName(rfc.getCiClassName());
+      ci.getAttrProps().putAll(rfc.getCiAttrProps());
+      for (Map.Entry<String, String> attr : rfc.getCiAttributes().entrySet()) {
+        ci.addCiAttribute(attr.getKey(), attr.getValue());
+      }
+      wo.setResultCi(ci);
+      wo.setDpmtRecordState("complete");
+      responseMsg.setStringProperty("task_result_code", "200");
+    } else {
+      wo.setDpmtRecordState("failed");
+      responseMsg.setStringProperty("task_result_code", "404");
+      wo.setComments("Failed by inductor stub");
     }
+    responseMsg.setStringProperty("type", "deploybom");
+    responseMsg.setText(gson.toJson(wo));
+    producer.send(responseMsg);
+    session.commit();
+  }
 
+  private void log(String message) {
+    System.out.println("Listener " + listenerId + " " + message);
+  }
 }
